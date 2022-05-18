4b4ab6fc389470710dbe3095775126c0cbf2b4d6
42Null <67847710+42null@users.noreply.github.com>
2022-05-18 02:15:11 +0000

    Ranged bullets and astroid id's now based on chronological instead of random.

--- src/server/collisions.js
+++ src/server/collisions.js
@@ -7,6 +7,21 @@ function applyBulletCollisions(players, astroids, bullets) {//TODO: Good off of 
   for (let i = 0; i < bullets.length; i++) {//TODO: Make it stop if all bullets are destroyed on players
     // Look for a player (who didn't create the bullet) to collide each bullet with.
     // As soon as we find one, break out of the loop to prevent double counting a bullet.
+
+  // For astroids
+    for (let j = 0; j < astroids.length; j++) {
+      const bullet = bullets[i];
+      const astroid = astroids[j];
+      if(astroid.distanceTo(bullet) <= Math.trunc(Math.log(astroid.hp+3)*12) + Constants.BULLET_RADIUS){
+        bullet.hit = "A"+astroid.type;
+        destroyedBullets.push(bullet);
+        if(astroid.takeBulletDamage()){
+            destroyedAstroids.push(astroid);
+        }
+        break;
+      }
+    }
+      
     for (let j = 0; j < players.length; j++) {
       const bullet = bullets[i];
       const player = players[j];
@@ -20,20 +35,12 @@ function applyBulletCollisions(players, astroids, bullets) {//TODO: Good off of 
         break;
       }
     }
-// For astroids
-    for (let j = 0; j < astroids.length; j++) {
-      const bullet = bullets[i];
-      const astroid = astroids[j];
-      if(astroid.distanceTo(bullet) <= Math.trunc(Math.log(astroid.hp+3)*12) + Constants.BULLET_RADIUS){
-        bullet.hit = "A"+1.4;
-        destroyedBullets.push(bullet);
-        if(astroid.takeBulletDamage()){
-            destroyedAstroids.push(astroid);
-        }
-        break;
-      }
-    }
+
+//TODO: Make remove destroyed bullets sooner so that counting up of destroyedBullets is not wastefull.
       
+    for (let j = 0; j < bullets.length; j++) {
+      // bullets[i].timeLeft += 1;// = bullets[i];
+    }
   }
   return{
     destroyedBullets: destroyedBullets,
